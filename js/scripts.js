f7916b1adf0b1e9df0b61c8e8fa12937669d3e6e
Tiffany Siu <tsiu88@gmail.com>
2020-01-15 17:02:52 -0800

    add multiple forms and buttons to alernate conversion

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,53 @@
-function convertFahrenheitToCelsius(temp){
-  var result = parseInt(temp);
-  alert((result -32)* 5/9);
-}
-
-function convertCelsiusToFahrenheit(temp){
-  var result = parseInt(temp);
-  alert(result * (9/5) + 32);
-}
-
-function convertTemp(input, temp){
-  if(input){
-  convertFahrenheitToCelsius(temp);
-  }
-  else{
-  convertCelsiusToFahrenheit(temp);
-  }
-}
-
 function convertGalToL(volume){
   return volume * 3.78541;
 }
 
+function convertLToGal(volume){
+  return volume / 3.78541;
+}
+
+function convertFahrenheitToCelsius(temp){
+  return (temp -32)* 5/9;
+}
+
+function convertCelsiusToFahrenheit(temp){
+  return temp * (9/5) + 32;
+}
+
+
 $(document).ready(function(){
-  
-  //var inputTemp = prompt("Temperature?");
-  //var inputType = confirm("Temp in F?");
 
-  //convertTemp(inputType, inputTemp);
+  var clickedId;
+  $(".btn").click(function(){
+    clickedId = $(this).attr("id");
+  });
 
-  $("#galToL").submit(function(event){
+  $("#galAndL").submit(function(event){
     event.preventDefault();
-    var volume = parseInt($("#gal").val());
-    console.log(convertGalToL(volume));
-    var result = convertGalToL(volume);
+    var volume = parseInt($("#galOrL").val());
+    var result;
+    
+    if(clickedId === "galToL"){
+      result = convertGalToL(volume);
+    }
+    if(clickedId === "lToGal"){
+      result = convertLToGal(volume);
+    }
+
+    $("#output").text(result);
+  });
+
+  $("#cAndF").submit(function(event){
+    event.preventDefault();
+    var temp = parseInt($("#cOrF").val());
+    var result;
+    
+    if(clickedId === "fToC"){
+      result = convertFahrenheitToCelsius(temp);
+    }
+    if(clickedId === "cToF"){
+      result = convertCelsiusToFahrenheit(temp);
+    }
 
     $("#output").text(result);
   });
